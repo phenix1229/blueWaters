de0c0df8f6442902c6983ee43defc956b4ad5c17
unknown <paul.garay@codeimmersives.com>
2023-04-18 21:42:46 -0400

    removed unnecessary routes

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 require('../../lib/passport');
 
 const userController = require('./controllers/userController');
-const userValidation = require('./utils/userValidation');
 
 //register new member
 router.post('/saveMember', userController.saveMember);
@@ -46,14 +45,6 @@ router.get('/reports', userController.reportsPage);
 
 //render monthly invoice page
 router.get('/monthlyInvoice', userController.monthlyInvoicePage);
-
-// router.post('/checkMember', userController.checkMember);
-
-//render member info page
-// router.get('/memberInfo', userController.memberInfoPage);
-
-//render login error page
-// router.get('/loginError', userController.loginError);
 
 //update member
 router.post('/updateMember', userController.updateMember);
