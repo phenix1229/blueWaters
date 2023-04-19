7ebba12d8ce5d0cbef573e306efc406b76334e33
unknown <paul.garay@codeimmersives.com>
2023-04-18 21:38:46 -0400

    removed unnecessary files

--- routes/accounts/models/SavingsStatement.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SavingsStatementSchema = new mongoose.Schema({
-    owner:{type:mongoose.Schema.Types.ObjectId, ref:'Savings'},
-    month:{type:String},
-    transactions:[]
-});
-
-module.exports = mongoose.model('SavingsStatement', SavingsStatementSchema);
